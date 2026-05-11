95a5b60aa0be21863a8008ee758615a9a46708f8
SarunMr <sarunmaharjan38@gmail.com>
2026-05-11 18:37:56 +0545

    start of the project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// A simple GET route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello! World");
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️ Server is running at http://localhost:${PORT}`);
+});
