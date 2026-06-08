2778cc0fb5fa5c3ffef6a1a295214b0a98cdaad1
SarunMr <sarunmaharjan38@gmail.com>
2026-06-08 20:08:15 +0545

    created data tranfer object

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { RegisterSchema, LoginSchema } from "../types/auth.type";
+
+export const RegisterDTO = RegisterSchema.pick({
+  username: true,
+  email: true,
+  password: true,
+  role: true,
+});
+export type RegisterDTO = z.infer<typeof RegisterDTO>;
+
+export const LoginDTO = LoginSchema.pick({
+  email: true,
+  password: true,
+});
+export type LoginDTO = z.infer<typeof LoginDTO>;
