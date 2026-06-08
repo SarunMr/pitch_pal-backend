f6d6ad2afcf821d499901cb5cdbcd6e12bac80b3
SarunMr <sarunmaharjan38@gmail.com>
2026-06-08 20:40:03 +0545

    added some package

--- index.ts
+++ index.ts
@@ -2,9 +2,6 @@ import app from "./src/app.ts";
 import { PORT } from "./src/config/constant.ts";
 import connectDB from "./src/database/mongodb";
 
-import dotenv from "dotenv";
-dotenv.config();
-
 const start = async (): Promise<void> => {
   await connectDB();
 
