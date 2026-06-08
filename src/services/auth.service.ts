ec6a589e1df2dcaaffab30c480d5dd31c26c58d5
SarunMr <sarunmaharjan38@gmail.com>
2026-06-08 20:48:41 +0545

    login and register logic logic added with password encryption

---
+++ src/services/auth.service.ts
@@ -0,0 +1,56 @@
+import { UserMongoRepository } from "../repositories/auth.repository";
+import { IUser } from "../models/auth.model";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { LoginDTO, RegisterDTO } from "../dtos/auth.dto";
+import { HttpException } from "../exceptions/http-exception";
+import { SECRET_KEY } from "../config/constant";
+
+const userRepository = new UserMongoRepository();
+
+export class AuthService {
+  async register(userData: RegisterDTO): Promise<IUser> {
+    const existingEmail = await userRepository.getUserByEmail(userData.email);
+    if (existingEmail) {
+      throw new HttpException(409, "Email already in use");
+    }
+
+    const existingUsername = await userRepository.getUserByUsername(
+      userData.username,
+    );
+    if (existingUsername) {
+      throw new HttpException(409, "Username already taken");
+    }
+
+    const hashedPassword = await bcryptjs.hash(userData.password, 10);
+    const user = await userRepository.createUser({
+      ...userData,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+
+  async login(loginData: LoginDTO): Promise<{ user: IUser; token: string }> {
+    const user = await userRepository.getUserByEmail(loginData.email);
+    if (!user) {
+      throw new HttpException(401, "Invalid credentials");
+    }
+
+    const isPasswordValid = await bcryptjs.compare(
+      loginData.password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new HttpException(401, "Invalid credentials");
+    }
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email, role: user.role },
+      SECRET_KEY as string,
+      { expiresIn: "7d" },
+    );
+
+    return { user, token };
+  }
+}
