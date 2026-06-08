c3a96308fa9de7534a2c25b3eadf8602d272aa06
SarunMr <sarunmaharjan38@gmail.com>
2026-06-08 21:40:00 +0545

    used zod in controller

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { AuthService } from "../services/auth.service";
 import { LoginDTO, RegisterDTO } from "../dtos/auth.dto";
+import { HttpException } from "../exceptions/http-exception";
+import z from "zod";
 
 const authService = new AuthService();
 
@@ -11,8 +13,11 @@ export class AuthController {
     next: NextFunction,
   ): Promise<void> {
     try {
-      const body: RegisterDTO = req.body;
-      const user = await authService.register(body);
+      const body = RegisterDTO.safeParse(req.body);
+      if (!body.success) {
+        return next(new HttpException(400, z.prettifyError(body.error)));
+      }
+      const user = await authService.register(body.data);
       res.status(201).json({
         message: "Registration successful",
         data: {
@@ -29,8 +34,11 @@ export class AuthController {
 
   async login(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      const body: LoginDTO = req.body;
-      const { user, token } = await authService.login(body);
+      const body = LoginDTO.safeParse(req.body);
+      if (!body.success) {
+        return next(new HttpException(400, z.prettifyError(body.error)));
+      }
+      const { user, token } = await authService.login(body.data);
       res.status(200).json({
         message: "Login successful",
         data: {
