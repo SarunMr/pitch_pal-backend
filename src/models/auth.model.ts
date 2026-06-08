51a32bd9c783606c372a747a9eaa4c34131bec26
SarunMr <sarunmaharjan38@gmail.com>
2026-06-08 19:53:29 +0545

    created replica for database

---
+++ src/models/auth.model.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { RegisterInput } from "../types/auth.type";
+
+export interface IUser extends Omit<RegisterInput, "password">, Document {
+  _id: mongoose.Types.ObjectId;
+  password: string;
+  profilePicture?: string;
+  firstName?: string;
+  lastName?: string;
+  bio?: string;
+  phone?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserMongoSchema: Schema = new Schema<IUser>(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["entrepreneur", "investor", "admin"],
+      default: "investor",
+    },
+
+    // Profile — optional, filled later
+    firstName: { type: String },
+    lastName: { type: String },
+    profilePicture: { type: String },
+    bio: { type: String },
+    phone: { type: String },
+
+    // googleId:    { type: String },
+    // authProvider:{ type: String, enum: ["local", "google"], default: "local" },
+    // isVerified:  { type: Boolean, default: false },
+    // kycStatus:   { type: String, enum: ["pending", "approved", "rejected"] },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const UserModel = mongoose.model<IUser>("User", UserMongoSchema);
