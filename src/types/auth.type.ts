4f62292247a2691cb4cbd41e857ce48652845f20
SarunMr <sarunmaharjan38@gmail.com>
2026-06-19 09:50:57 +0545

    updated auth.type to include profile page items

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -9,6 +9,11 @@ export const AuthUserSchema = z.object({
   email: z.email(),
   username: z.string(),
   role: RoleSchema,
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  profilePicture: z.string().optional(), // local file path URL
+  bio: z.string().max(500).optional(),
+  phone: z.string().optional(),
   // isVerified: z.boolean().optional(),
   // kycStatus: z.enum(["pending", "approved", "rejected"]).optional(),
 });
@@ -26,4 +31,15 @@ export const LoginSchema = z.object({
   email: z.email("Invalid email address"),
   password: z.string().min(1, "Password is required"),
 });
+
 export type LoginInput = z.infer<typeof LoginSchema>;
+
+export const UpdateProfileSchema = z.object({
+  firstName: z.string().min(1, "First name is required"),
+  lastName: z.string().min(1, "Last name is required"),
+  username: z.string().min(3, "Username must be at least 3 characters"),
+  profilePicture: z.string().optional(),
+  bio: z.string().max(500).optional(),
+  phone: z.string().optional(),
+});
+export type UpdateProfileInput = z.infer<typeof UpdateProfileSchema>;
